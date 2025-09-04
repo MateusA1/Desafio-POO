40f99d02d197773c32fbc128314be3421504b6b5
MateusA1 <122369653+MateusA1@users.noreply.github.com>
2025-09-04 20:35:45 -0300

    Add files via upload

---
+++ q1´/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Pessoa
+{
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Pessoa pessoa1 = new Pessoa();
+        Console.WriteLine("Objeto pessoa 1 criado com sucesso!");
+    }
+}
