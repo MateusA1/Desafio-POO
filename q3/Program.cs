76aa51dfd01d8ecf60d2f0c7f057d75c38047293
MateusA1 <122369653+MateusA1@users.noreply.github.com>
2025-09-04 20:48:16 -0300

    Add files via upload

---
+++ q3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Animal
+{
+    public string Nome;
+    public int Idade;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal animal1 = new Animal();
+        animal1.Nome = "Leão";
+        animal1.Idade = 5;
+
+        Console.WriteLine("Animal: " + animal1.Nome + ", Idade: " + animal1.Idade);
+    }
+}
