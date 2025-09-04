3fe64b7382dc3f9d4c148111c4f5ec7b44a5bc31
MateusA1 <122369653+MateusA1@users.noreply.github.com>
2025-09-04 20:42:41 -0300

    Add files via upload

---
+++ q2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Carro
+{
+    public string Cor = "Vermelho";
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Carro meuCarro = new Carro();
+        Console.WriteLine("A cor do carro é: " + meuCarro.Cor);
+    }
+}
