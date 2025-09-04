2a41711cfdb8e568fc9196140db7b3978d848e3d
MateusA1 <122369653+MateusA1@users.noreply.github.com>
2025-09-04 20:58:45 -0300

    Add files via upload

---
+++ q5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Quadrado
+{
+    public double Lado;
+
+    public double Area()
+    {
+        return Lado * Lado;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Quadrado q = new Quadrado();
+        q.Lado = 4;
+
+        Console.WriteLine("Área do quadrado: " + q.Area());
+    }
+}
