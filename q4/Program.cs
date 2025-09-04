98d1747fae4c3b6aa83ed9eaa991da8be920ab11
MateusA1 <122369653+MateusA1@users.noreply.github.com>
2025-09-04 20:53:34 -0300

    Add files via upload

---
+++ q4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Livro
+{
+    public string Titulo;
+    public string Autor;
+
+    public void ExibirInfo()
+    {
+        Console.WriteLine("Título: " + Titulo + ", Autor: " + Autor);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Livro livro1 = new Livro();
+        livro1.Titulo = "Dom Casmurro";
+        livro1.Autor = "Machado de Assis";
+
+        livro1.ExibirInfo();
+    }
+}
