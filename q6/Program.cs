f76923b53c2eadbcebfd505e7ce86e65467c570a
MateusA1 <122369653+MateusA1@users.noreply.github.com>
2025-09-04 21:01:21 -0300

    Add files via upload

---
+++ q6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Aluno
+{
+    public string Nome;
+    public double Nota;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aluno a1 = new Aluno { Nome = "João", Nota = 8.5 };
+        Aluno a2 = new Aluno { Nome = "Maria", Nota = 9.0 };
+
+        if (a1.Nota > a2.Nota)
+            Console.WriteLine(a1.Nome + " tem a maior nota.");
+        else if (a2.Nota > a1.Nota)
+            Console.WriteLine(a2.Nome + " tem a maior nota.");
+        else
+            Console.WriteLine("As notas são iguais.");
+    }
+}
