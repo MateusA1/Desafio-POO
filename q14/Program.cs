3a1d42675a98f9c4550f0b05ffafd30e43744be5
MateusA1 <122369653+MateusA1@users.noreply.github.com>
2025-09-04 21:44:43 -0300

    Add files via upload

---
+++ q14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Veiculo
+{
+    public string Marca;
+    public string Modelo;
+
+    public Veiculo(string marca, string modelo)
+    {
+        Marca = marca;
+        Modelo = modelo;
+    }
+
+    public void Acelerar()
+    {
+        Console.WriteLine("Acelerando!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Veiculo v = new Veiculo("Ford", "Fiesta");
+        v.Acelerar();
+    }
+}
