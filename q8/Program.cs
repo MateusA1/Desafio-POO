507491d87b698873b4fdab58e5b418cf3258448d
MateusA1 <122369653+MateusA1@users.noreply.github.com>
2025-09-04 21:08:52 -0300

    Add files via upload

---
+++ q8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class ContaBancaria
+{
+    public double Saldo = 0;
+
+    public void Depositar(double valor)
+    {
+        Saldo += valor;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ContaBancaria conta = new ContaBancaria();
+        conta.Depositar(500);
+
+        Console.WriteLine("Saldo: " + conta.Saldo);
+    }
+}
