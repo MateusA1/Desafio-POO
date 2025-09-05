c0dfe03ce7c06393f8d415f67c892d6600e2ce42
MateusA1 <122369653+MateusA1@users.noreply.github.com>
2025-09-04 23:37:58 -0300

    Add files via upload

---
+++ q17/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Biblioteca
+{
+    private List<string> livros = new List<string>();
+
+    public void AdicionarLivro(string titulo)
+    {
+        livros.Add(titulo);
+    }
+
+    public void ExibirLivros()
+    {
+        Console.WriteLine("Livros na biblioteca:");
+        foreach (string livro in livros)
+        {
+            Console.WriteLine("- " + livro);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Biblioteca b = new Biblioteca();
+        b.AdicionarLivro("Dom Casmurro");
+        b.AdicionarLivro("Memórias Póstumas");
+        b.ExibirLivros();
+    }
+}
