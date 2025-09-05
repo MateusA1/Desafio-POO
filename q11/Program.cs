2f6520953144ee9428b176da6f62bb0aaf473ad0
MateusA1 <122369653+MateusA1@users.noreply.github.com>
2025-09-04 21:28:21 -0300

    Add files via upload

---
+++ q11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Retangulo
+{
+    public double Largura;
+    public double Altura;
+
+    public Retangulo(double largura, double altura)
+    {
+        Largura = largura;
+        Altura = altura;
+    }
+
+    public double Area()
+    {
+        return Largura * Altura;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Retangulo r = new Retangulo(5, 3);
+        Console.WriteLine("Área do retângulo: " + r.Area());
+    }
+}
