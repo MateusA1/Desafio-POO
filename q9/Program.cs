83cfec53961ad3099c176def99b09f6f882d707b
MateusA1 <122369653+MateusA1@users.noreply.github.com>
2025-09-04 21:20:53 -0300

    Add files via upload

---
+++ q9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Produto
+{
+    public string Nome;
+    public double Preco;
+
+    public void AplicarDesconto()
+    {
+        Preco -= Preco * 0.10;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Produto p = new Produto();
+        p.Nome = "Notebook";
+        p.Preco = 3000;
+
+        p.AplicarDesconto();
+
+        Console.WriteLine("Produto: " + p.Nome + ", Preço com desconto: " + p.Preco);
+    }
+}
