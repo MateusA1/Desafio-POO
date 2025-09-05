7c3f9a75142df74e2ca089e8c9eef0dd554a24ac
MateusA1 <122369653+MateusA1@users.noreply.github.com>
2025-09-04 23:52:39 -0300

    Add files via upload

---
+++ q18/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Calculadora
+{
+    public double Somar(double a, double b) => a + b;
+    public double Subtrair(double a, double b) => a - b;
+    public double Multiplicar(double a, double b) => a * b;
+    public double Dividir(double a, double b) => a / b;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculadora calc = new Calculadora();
+        Console.WriteLine("Soma: " + calc.Somar(5, 3));
+        Console.WriteLine("Divisão: " + calc.Dividir(10, 2));
+    }
+}
