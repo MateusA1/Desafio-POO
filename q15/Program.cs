bc27d6ddfb7aed72535ef75ff8d1701995b94a1c
MateusA1 <122369653+MateusA1@users.noreply.github.com>
2025-09-04 21:47:36 -0300

    Add files via upload

---
+++ q15/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Triangulo
+{
+    public double Lado1, Lado2, Lado3;
+
+    public Triangulo(double lado1, double lado2, double lado3)
+    {
+        Lado1 = lado1;
+        Lado2 = lado2;
+        Lado3 = lado3;
+    }
+
+    public double Perimetro()
+    {
+        return Lado1 + Lado2 + Lado3;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Triangulo t = new Triangulo(3, 4, 5);
+        Console.WriteLine("Perímetro do triângulo: " + t.Perimetro());
+    }
+}
