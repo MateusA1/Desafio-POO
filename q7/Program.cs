1c593f413142ca7d6e7d5add5ee82293dcc412d0
MateusA1 <122369653+MateusA1@users.noreply.github.com>
2025-09-04 21:06:06 -0300

    Add files via upload

---
+++ q7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Circulo
+{
+    public double Raio;
+
+    public double Area()
+    {
+        return Math.PI * Raio * Raio;
+    }
+
+    public double Perimetro()
+    {
+        return 2 * Math.PI * Raio;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Circulo c = new Circulo();
+        c.Raio = 3;
+
+        Console.WriteLine("Área: " + c.Area());
+        Console.WriteLine("Perímetro: " + c.Perimetro());
+    }
+}
