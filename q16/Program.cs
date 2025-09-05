bbbdd674aeadb7320b4a95df99a6b0e5efb9f26d
MateusA1 <122369653+MateusA1@users.noreply.github.com>
2025-09-04 21:51:29 -0300

    Add files via upload

---
+++ q16/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Usuario
+{
+    public string Nome;
+    public string Email;
+
+    public Usuario(string nome, string email)
+    {
+        Nome = nome;
+        Email = email;
+    }
+
+    public void AlterarEmail(string novoEmail)
+    {
+        Email = novoEmail;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Usuario u = new Usuario("Ana", "ana@email.com");
+        u.AlterarEmail( "mateusTicx@email.com");
+        Console.WriteLine("Novo email: " + u.Email);
+    }
+}
