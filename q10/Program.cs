7e8bd1c4ddfaca877b1a6d4328af13b375d1d7be
MateusA1 <122369653+MateusA1@users.noreply.github.com>
2025-09-04 21:24:16 -0300

    Add files via upload

---
+++ q10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Ponto
+{
+    public double X;
+    public double Y;
+
+    public double DistanciaOrigem()
+    {
+        return Math.Sqrt(X * X + Y * Y);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Ponto p = new Ponto();
+        p.X = 3;
+        p.Y = 4;
+
+        Console.WriteLine("Distância até a origem: " + p.DistanciaOrigem());
+    }
+}
