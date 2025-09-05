d189f0541896269bdcc0afaedaf7fd3afdb6ee11
MateusA1 <122369653+MateusA1@users.noreply.github.com>
2025-09-04 21:35:42 -0300

    Add files via upload

---
+++ q13/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Data
+{
+    public int Dia;
+    public int Mes;
+    public int Ano;
+
+    public Data(int dia, int mes, int ano)
+    {
+        Dia = dia;
+        Mes = mes;
+        Ano = ano;
+    }
+
+    public string Formatar()
+    {
+        return $"{Dia:D2}/{Mes:D2}/{Ano}";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Data d = new Data(4, 9, 2025);
+        Console.WriteLine("Data formatada: " + d.Formatar());
+    }
+}
