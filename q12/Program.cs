43757bdfd52435aae4020d0d546c8b03ca10fca9
MateusA1 <122369653+MateusA1@users.noreply.github.com>
2025-09-04 21:33:15 -0300

    Add files via upload

---
+++ q12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Funcionario
+{
+    public string Nome;
+    public double Salario;
+
+    public Funcionario(string nome, double salario)
+    {
+        Nome = nome;
+        Salario = salario;
+    }
+
+    public void AumentarSalario(double percentual)
+    {
+        Salario += Salario * (percentual / 100);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Funcionario f = new Funcionario("Carlos", 2000);
+        f.AumentarSalario(10);
+        Console.WriteLine("Salário após aumento: " + f.Salario);
+    }
+}
